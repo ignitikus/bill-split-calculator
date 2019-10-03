0e4e9c0dfe268213725e154b4aaff93aba206eee
NikolayK <nikolay.kim@codeimmersive.com>
2019-10-03 15:19:48 -0400

    adds basic split bill converter

---
+++ billSplitCalculator.js
@@ -0,0 +1,25 @@
+const overall = parseFloat(process.argv[2]);
+const tip = parseFloat(process.argv[3]);
+const party = parseFloat(process.argv[4]);
+
+function tipOutput(tip){
+    if (tip >=25){
+        return " What a generous tip!";
+    } else if (tip >=10 && tip <25){
+        return " Thank you, come again!";
+    } else {
+        return " Thanks, I guess.";
+    }
+}
+
+function noParty (party){
+    if (party >= 1) {
+    return party;
+    } else {
+        return party = 1;
+    }
+}
+
+const total = overall * (tip) / 100 + overall;
+const result = total / noParty(party);
+console.log(result + tipOutput(tip))
